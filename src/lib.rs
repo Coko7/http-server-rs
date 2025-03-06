a8e0fc24bd4b87a0beced4216c53a3d518386b47
Coko <91132775+Coko7@users.noreply.github.com>
2025-03-06 17:17:13 +0100

    feat: mark all mods as pub to use in other projs + change name in Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-mod cookie;
-mod http;
-mod http_request;
-mod http_response;
-mod http_response_builder;
-mod thread_pool;
-mod web_server;
+pub mod cookie;
+pub mod http;
+pub mod http_request;
+pub mod http_response;
+pub mod http_response_builder;
+pub mod thread_pool;
+pub mod web_server;
